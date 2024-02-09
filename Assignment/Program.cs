16bdfbdd20be63728f586915738f4d3fc344c911
Mohammedmassod <mohammedmassowd@gmail.com>
2024-02-09 22:53:01 +0300

    #test

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddScoped<IEmployeeRepository,EmployeeRepository>();
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
