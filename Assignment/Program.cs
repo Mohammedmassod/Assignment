ac359387eb169901f5f45a9d9714e9975e838c88
Mohammedmassod <mohammedmassowd@gmail.com>
2024-02-09 22:48:51 +0300

    #AddScoped Service And Repository

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,4 +1,7 @@
 using Assignment.DB;
+using Assignment.Repository;
+using Assignment.Service;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +13,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IEmployeeService,EmployeeService>();
+builder.Services.AddScoped<IEmployeeRepository,EmployeeRepository>();
 
 var app = builder.Build();
 
